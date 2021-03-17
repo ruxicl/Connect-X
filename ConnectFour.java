af370f4844de71780bc9529fdc575387598dbaa0
Ruxandra Icleanu <s2011447@ed.ac.uk>
2021-03-17 15:58:04 +0200

    Update ConnectFour.java

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,18 +1,6 @@
-/**l
- * The main class of the Connect Four game.
- * You should not have to touch this code (except maybe for advanced features).
- *
- * @author David Symons
- */
 public final class ConnectFour
 {
-	/**
-	 * The code provided for this assignment follows a design pattern called
-	 * Model-View-Controller (MVC). The main method instantiates each of these
-	 * components and then starts the game loop.
-	 *
-	 * @param args No arguments expected.
-	 */
+	
 	public static void main(String[] args)
 	{
 		// Creates a model representing the state o1f the game.
